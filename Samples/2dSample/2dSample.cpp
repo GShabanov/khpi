3cdcd5eee973f176a0b8358c9a0edaa762b6bed5
Gennady Shabanov <gennady.shabanov@gmail.com>
2025-06-13 21:32:25 +0300

    Math

--- Samples/2dSample/2dSample.cpp
+++ Samples/2dSample/2dSample.cpp
@@ -257,6 +257,7 @@ OnCommand(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+
 bool
 CalcAngles(float BD2, float angle1, float angle2, float& gamma, float& lambda, float& fi)
 {
@@ -264,6 +265,74 @@ CalcAngles(float BD2, float angle1, float angle2, float& gamma, float& lambda, f
 
     float BD = sqrtf(BD2);
 
+    //-----------------------
+    // first triangle
+    //-----------------------
+    float a = (BD2 + AB2 - AD2) / (2 * BD * AB);  // betta1
+    float b = (BD2 + AD2 - AB2) / (2 * BD * AD);  // gamma1
+
+    if (a > 1.0 || a < -1.0)
+        return false;
+
+    float betta1 = 0.0;
+    float gamma1 = 0.0;
+
+    //
+    // We calculate the arccosine on the larger side, thus we reduce the calculation error
+    //
+    if (_abs(a) > _abs(b))
+    {
+        betta1 = acosf(a);
+        gamma1 = M_PI - (alfa1 + betta1);
+    }
+    else
+    {
+        gamma1 = acosf(b);
+        betta1 = M_PI - (alfa1 + gamma1);
+    }
+
+
+    //-----------------------
+    // Second triangle
+    //-----------------------
+    float gamma2 = 0.0;
+
+    a = (BD2 + CD2 - BC2) / (2 * BD * CD);
+
+    if (a > 1.0 || a < -1.0)
+        return false;
+
+    gamma2 = acosf(a);
+
+    gamma = gamma1 + gamma2;
+
+
+
+    float betta2 = 0.0;
+    //float lambda = 0.0;
+
+    a = (BD2 + BC2 - CD2) / (2 * BD * BC);   // betta2
+    b = (BC2 + CD2 - BD2) / (2 * BC * CD);   // lambda
+
+    if (a > 1.0 || a < -1.0)
+        return false;
+
+
+    if (_abs(a) > _abs(b))
+    {
+        betta2 = acosf(a);
+        lambda = M_PI - (gamma2 + betta2);
+    }
+    else
+    {
+        lambda = acosf(b);
+        betta2 = M_PI - (gamma2 + lambda);
+    }
+
+    return true;
+
+    ////////////////
+    /*
     float a = (BD2 + AB2 - AD2) / (2 * BD * AB);  // gamma
     float b = (BD2 + AD2 - AB2) / (2 * BD * AD);  // fi
 
@@ -334,14 +403,14 @@ CalcAngles(float BD2, float angle1, float angle2, float& gamma, float& lambda, f
     //
     // check if to close
     //
-    /*if ((gamma1 + gamma2) < 0.9)
-        return false;*/
+    if ((gamma1 + gamma2) < 0.9)
+        return false;
 
 
     //
     // Last calculations. We need the outer angles of the triangle.
     //
-    fi = M_PI - (fi1 + fi2);
+    fi = M_PI - (fi1 + fi2);*/
 
     return true;
 }
@@ -411,7 +480,7 @@ OnPaint(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     float gamma, fi, lambda;
 
-    if (CalcAngles(BD2, angle1, angle2, gamma, fi, lambda) != true) {
+    if (CalcAngles(BD2, angle1, angle2, gamma, lambda, fi) != true) {
         EndPaint(hWnd, &ps);
 
         return 0;
@@ -431,8 +500,11 @@ OnPaint(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     MoveToEx(hdc, (int)(baseX + Cx * 2), (int)(baseY + Cy * 2), (LPPOINT)NULL);
 
-    float  D2x = Cx + (CD + 50.0f) * cosf(lambda - (gamma - angle2));
-    float  D2y = Cy + (CD + 50.0f) * sinf(lambda - (gamma - angle2));
+    //float  D2x = Cx + (CD + 50.0f) * cosf(lambda - (gamma - angle2));
+    //float  D2y = Cy + (CD + 50.0f) * sinf(lambda - (gamma - angle2));
+
+    float  D2x = Cx + (CD + 50.0f) * cosf(angle2);
+    float  D2y = Cy + (CD + 50.0f) * sinf(angle2);
 
     LineTo(hdc, (int)(baseX + D2x * 2), (int)(baseY + D2y * 2));
 
